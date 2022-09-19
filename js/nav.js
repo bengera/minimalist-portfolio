66f1a1d3c89502316c9181b50490a45a9aaa918e
unknown <bengera89@gmail.com>
2022-09-19 21:11:06 +0700

    navigation-mobile

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+const hamMenu = document.querySelector('.hamburger-menu');
+const navUL = document.querySelector('.nav-container');
+
+hamMenu.addEventListener('click',() => {
+    console.log('toggle-menu');
+    hamMenu.classList.toggle('active');
+    navUL.classList.toggle('show');
+    
+    
+});

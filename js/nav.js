0e9cc4757750b6ba24d54a8bf86ef0c8fa565ea2
Ben <bengeraghty.bg@gmail.com>
2022-10-07 10:38:54 +0700

    homepage-finish

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,48 @@
 const hamMenu = document.querySelector('.hamburger-menu');
 const navUL = document.querySelector('.nav-container');
 
-hamMenu.addEventListener('click',() => {
-    console.log('toggle-menu');
+hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     navUL.classList.toggle('show');
-    
-    
-});
 
+
+});
+// GITHUB
+const githubIcon = document.getElementById('githubIcon');
+
+githubIcon.addEventListener('mouseover', () => {
+    githubIcon.src = "./images/icons/github-hover.svg"
+
+})
+
+githubIcon.addEventListener('mouseout', () => {
+    githubIcon.src = "./images/icons/github.svg"
+
+})
+
+//TWITTER
+const twitterIcon = document.getElementById('twitterIcon');
+
+twitterIcon.addEventListener('mouseover', () => {
+    twitterIcon.src = "./images/icons/twitter-hover.svg"
+
+})
+
+twitterIcon.addEventListener('mouseout', () => {
+    twitterIcon.src = "./images/icons/twitter.svg"
+
+})
+
+//LINKEDIN
+
+const linkedinIcon = document.getElementById('linkedinIcon');
+
+linkedinIcon.addEventListener('mouseover', () => {
+    linkedinIcon.src = "./images/icons/linkedin-hover.svg"
+
+})
+
+linkedinIcon.addEventListener('mouseout', () => {
+    linkedinIcon.src = "./images/icons/linkedin.svg"
+
+})

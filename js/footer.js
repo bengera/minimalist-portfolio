070df7939e36df1c9904cd49b1e8bf2b3f8e30f9
Ben <bengeraghty.bg@gmail.com>
2022-10-07 12:03:12 +0700

    class-elements

---
+++ js/footer.js
@@ -0,0 +1,46 @@
+class Footer extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+
+  connectedCallback() {
+    this.innerHTML = `
+    <footer class="footer">
+    <div class="footer__inner-container">
+    <div class="footer__left-content">
+      <img src="./images/logo-white.svg" alt="" class="navigation-footer__logo">
+      <nav class="footer__nav">
+        <ul class="footer__nav-list">
+          <li class="footer__nav-item"><a href="#" class="footer__nav-link">Home</a></li>
+          <li class="footer__nav-item"><a href="#" class="footer__nav-link">Portfolio</a></li>
+          <li class="footer__nav-item"><a href="#" class="footer__nav-link">Contact me</a></li>
+        </ul>
+      </nav>
+    </div>
+    <div class="footer__right-content">
+      <div class="footer__social-container">
+        <img id="githubIcon" src="./images/icons/github.svg" alt="github" class="footer__icon">
+        <img id="twitterIcon" src="./images/icons/twitter.svg" alt="twitter" class="footer__icon">
+        <img id="linkedinIcon" src="./images/icons/linkedin.svg" alt="linkedin" class="footer__icon">
+      </div>
+    </div>
+  </div>
+  </footer>
+    `
+
+  
+  }
+
+}
+
+  customElements.define('footer-component', Footer);
+
+
+
+
+
+
+
+
+
---
+++ js/navigation.js
@@ -0,0 +1,31 @@
+class Navigation extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+
+  connectedCallback() {
+    this.innerHTML = `
+    <nav class="navigation-top">
+    <img src="./images/logo.svg" alt="" class="navigation-top__logo">
+    <button class="hamburger-menu"></button>
+    <div class="nav-container">
+      <ul class="nav-primary">
+        <li class="nav-primary__list-item"><a href="#" class="nav-primary__list-link cyan">Home</a></li>
+        <li class="nav-primary__list-item"><a href="#" class="nav-primary__list-link">Portfolio</a></li>
+        <li class="nav-primary__list-item"><a href="#" class="nav-primary__list-link">Contact me</a></li>
+      </ul>
+    </div>
+  </nav>
+    `
+
+  
+  }
+
+}
+
+
+  customElements.define('navigation-component', Navigation);
+
+
+

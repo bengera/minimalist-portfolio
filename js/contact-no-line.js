a2e8aa2e0d93c1279da4fa855b5935067faab3d7
Ben <bengeraghty.bg@gmail.com>
2022-10-18 11:55:06 +0700

    portfolio-tablet

--- js/contact-no-line.js
+++ js/contact-no-line.js
@@ -1,7 +1,9 @@
 const contact = document.getElementById('contact-section')
+const home = document.getElementById('home-link');
 
 window.addEventListener('load', () => {
     console.log('remove-line and margin');
     contact.style.borderTop = 0;
     contact.style.marginTop = 0;
 })
+
--- js/navigation.js
+++ js/navigation.js
@@ -11,7 +11,7 @@ class Navigation extends HTMLElement {
     <button class="hamburger-menu"></button>
     <div class="nav-container">
       <ul class="nav-primary">
-        <li class="nav-primary__list-item"><a href="#" class="nav-primary__list-link cyan">Home</a></li>
+        <li class="nav-primary__list-item"><a href="#" id="home-link" class="nav-primary__list-link cyan">Home</a></li>
         <li class="nav-primary__list-item"><a href="#" class="nav-primary__list-link">Portfolio</a></li>
         <li class="nav-primary__list-item"><a href="#" class="nav-primary__list-link">Contact me</a></li>
       </ul>

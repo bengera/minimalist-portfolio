210cb224e7559a9c373151b2626418ee57343ab4
Ben <bengeraghty.bg@gmail.com>
2022-10-18 19:38:37 +0700

    desktop-portfolio

--- js/navigation.js
+++ js/navigation.js
@@ -11,8 +11,8 @@ class Navigation extends HTMLElement {
     <button class="hamburger-menu"></button>
     <div class="nav-container">
       <ul class="nav-primary">
-        <li class="nav-primary__list-item"><a href="#" id="home-link" class="nav-primary__list-link cyan">Home</a></li>
-        <li class="nav-primary__list-item"><a href="#" class="nav-primary__list-link">Portfolio</a></li>
+        <li class="nav-primary__list-item"><a href="./index.html" id="home-link" class="nav-primary__list-link cyan">Home</a></li>
+        <li class="nav-primary__list-item"><a href="./portfolio.html" class="nav-primary__list-link">Portfolio</a></li>
         <li class="nav-primary__list-item"><a href="#" class="nav-primary__list-link">Contact me</a></li>
       </ul>
     </div>
